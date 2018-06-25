e10ac258cf785994c039a2fc8bf955f7eadd9ae2
Glassman <mglassmn@ad.unc.edu>
2018-06-25 16:33:56 -0400

    Building the game grid via a method of class Fourinarow containing for loops.

--- javascript/4inarow.js
+++ javascript/4inarow.js
@@ -4,9 +4,22 @@ class Fourinarow{
   constructor(selector){
     this.ROWS = 6;
     this.COLS = 7;
-
+    this.selector = selector;
+    this.createGrid();
   //Using jQuery to display what is currently happening in browser
   const $grid = $(selector);
   $grid.html('test');
+  }
+
+  //Adding a method that will build up a bunch of divs into grid
+  createGrid(){
+    const $board = $(this.selector);
+    //for loop creating the rows and columns
+    for (let row = 0; row < this.ROWS; row++){
+      const $row = $('<div>')
+        .addClass('row');
+      $board.append($row);
+    }
+    console.log($board.html());
   }
 }
