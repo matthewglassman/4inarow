69227064f73f45d39483ae2b5ee84595bdbd1820
Glassman <mglassmn@ad.unc.edu>
2018-06-25 15:38:43 -0400

    Added .gitignore file

--- javascript/4inarow.js
+++ javascript/4inarow.js
@@ -1,8 +1,12 @@
 //Declare a class to be reused in main.js file
 class Fourinarow{
-  //constructor is going to take in selector and pass it to 
+  //constructor is going to take in selector and pass it to
   constructor(selector){
     this.ROWS = 6;
     this.COLS = 7;
+
+  //Using jQuery to display what is currently happening in browser
+  const $grid = $(selector);
+  $grid.html('test');
   }
 }
