1bc12f0b0e30effb0e89b561777244264fe9fc1c
Glassman <mglassmn@ad.unc.edu>
2018-06-25 14:52:54 -0400

    Created Class Object Fourinarow and beginning instance of that object.

---
+++ javascript/4inarow.js
@@ -0,0 +1,8 @@
+//Declare a class to be reused in main.js file
+class Fourinarow{
+  //constructor is going to take in selector and pass it to 
+  constructor(selector){
+    this.ROWS = 6;
+    this.COLS = 7;
+  }
+}
---
+++ javascript/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  //Draw grid here by instantiating new grid object from 4inarow.js
+  const fourinarow = new Fourinarow('')
+})
